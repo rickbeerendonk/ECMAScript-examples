5460f875ab7230f299fce839dd546562de109b97
Rick Beerendonk <rick@beerendonk.com>
2015-06-02 13:54:38 +0200

    More Promise demos. Renamed ES2016 demos.

---
+++ src/40-promise-race.js
@@ -0,0 +1,20 @@
+"use strict";
+
+// Babel:
+require("babel/polyfill");
+
+var p1 = new Promise(function(resolve, reject) {
+	 	setTimeout(() => resolve("Resolved 1" ), 2000);
+	 });
+var p2 = new Promise(function(resolve, reject) { 
+		setTimeout(() => resolve("Resolved 2"), 1000); 
+	});
+var p3 = new Promise(function(resolve, reject) { 
+		setTimeout(() => reject("Rejected 3"), 1500); 
+	});
+
+Promise
+	.race([p1, p2, p3])
+	.then(x => console.log("Success: " + x)).catch(e => console.log("Fail: " + e));
+	
+console.log("End of file...");
--- src/40-promise.js
+++ src/40-promise.js
@@ -4,11 +4,17 @@
 require("babel/polyfill");
 
 var p1 = new Promise(function(resolve, reject) {
-	 	setTimeout(() => resolve("1"), 2000);
+	 	setTimeout(() => resolve("Resolved" ), 2000);
 	 });
 var p2 = new Promise(function(resolve, reject) { 
-		setTimeout(() => reject("2"), 1000); 
+		setTimeout(() => reject("Rejected"), 1000); 
 	});
 
-var r1 = p1.then(x => console.log("Success: " + x)).catch(e => console.log("Fail: " + e));
-var r2 = p2.then(x => console.log("Success: " + x)).catch(e => console.log("Fail: " + e));
+p1.then(x => console.log("Success: " + x)).catch(e => console.log("Fail: " + e));
+p2.then(x => console.log("Success: " + x)).catch(e => console.log("Fail: " + e));
+
+// Alternative syntax:
+p1.then(x => console.log("Success: " + x), e => console.log("Fail: " + e));
+p2.then(x => console.log("Success: " + x), e => console.log("Fail: " + e));
+
+console.log("End of file...");
