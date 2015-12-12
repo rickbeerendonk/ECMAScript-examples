fba587ff61c07728eb760bb9d1e7ae22b3351425
Rick Beerendonk <rick@beerendonk.com>
2015-12-12 22:35:29 +0100

    Correction

--- src/32-comprehensions.js
+++ src/32-comprehensions.js
@@ -3,5 +3,5 @@
 var result = [for (i of [ 1, 2, 3 ]) i * i ];
 
 console.log(result[0]);  // 1
-console.log(result[1]);  // 2
-console.log(result[2]);  // 3
+console.log(result[1]);  // 4
+console.log(result[2]);  // 9
