0782eaf95a415a46943b3aa5b722b6e4b6fcf66e
Rick Beerendonk <rick@beerendonk.com>
2016-01-15 21:08:01 +0100

    Correct incorrect comment.

--- src/80-map.js
+++ src/80-map.js
@@ -19,7 +19,7 @@ logIterator(map.values());   // 111; 222; 333
 logIterator(map.entries());  // A,111; B,222; C,333
 
 console.log("");
-console.log(map.get("A"));   // 1
+console.log(map.get("A"));   // 111
 console.log(map.size);       // 3
 
 map.delete("A");
