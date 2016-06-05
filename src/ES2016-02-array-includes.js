023bd5aee4ee101a40932b3f6f5360101d76d8df
Rick Beerendonk <rick@beerendonk.com>
2016-06-06 00:16:44 +0200

    Completed ES2016 examples. Updated Babel.

---
+++ src/ES2016-02-array-includes.js
@@ -0,0 +1,8 @@
+'use strict';
+
+console.log( ['a', 'b'].includes('a') ); // true
+console.log( ['a', 'b'].includes('c') ); // false
+
+console.log( [NaN].includes(NaN) ); // true
+
+console.log( [-0].includes(+0) ); // true
