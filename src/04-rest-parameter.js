01e901b220013898befdeae3ce637c381af60aea
Rick Beerendonk <rick@beerendonk.com>
2016-09-25 06:45:22 +0200

    Improved comments.

--- src/04-rest-parameter.js
+++ src/04-rest-parameter.js
@@ -2,9 +2,9 @@
 
 function test(...a) {
 	console.log('Rest:');
-	console.log(a);
-	console.log(a.length);
-	console.log(Array.isArray(a));   // [1, "EcmaScript", 2017]
+	console.log(a);   // [1, "EcmaScript", 2017]
+	console.log(a.length);  // 3
+	console.log(Array.isArray(a));  // true  
 	
 	// ES6 FireFox: SyntaxError: 'arguments' object may not be used in conjunction with a rest parameter
 	// ES6 Chrome, Edge & Babel: <ok>
