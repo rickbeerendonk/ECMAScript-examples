21ae6c1c74cf77b46e0d78c261b15386514c9dbc
Rick Beerendonk <rick@beerendonk.com>
2016-09-25 08:15:11 +0200

    Added 2nd case to trailing commas in functions.

--- src/ES2017-01-trailing-function-commas.js
+++ src/ES2017-01-trailing-function-commas.js
@@ -1,8 +1,10 @@
 'use strict';
 
+// Case 1: No error because of the trailing comma in parameters
 function test(value, ) {
 	console.log(value);
 }
 
-console.log('test(1)');
-test(1);  // 1 (no error because of the trailing comma)
+// Case 2: No error because of the trailing comma in arguments
+console.log('test(1, )');
+test(1, );  // 1
