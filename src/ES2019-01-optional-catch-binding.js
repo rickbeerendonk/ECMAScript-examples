305c7d20e887d68f5b324512282ff84c3a14a554
Rick Beerendonk <rick@beerendonk.com>
2018-06-24 11:55:10 +0200

    Add ES2019 Optional Catch Binding example.

---
+++ src/ES2019-01-optional-catch-binding.js
@@ -0,0 +1,20 @@
+/*! Mozilla Public License Version 2.0 !*/
+/*! Copyright © 2018 Rick Beerendonk   !*/
+
+'use strict';
+
+// Previous:
+
+try {
+    throw 'Something went wrong';
+} catch (err) {
+    console.log('There was an error (traditional catch)');
+}
+
+// 2019 also allows:
+
+try {
+    throw 'Something went wrong';
+} catch {
+    console.log('There was an error (2019 catch)');
+}
