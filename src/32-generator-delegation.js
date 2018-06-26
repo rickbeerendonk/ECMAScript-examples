8e1fcfbbadda69d1a4682c977512ba4980a7585f
Rick Beerendonk <rick@beerendonk.com>
2018-06-26 15:55:47 +0200

    Add generator delegation example.

---
+++ src/32-generator-delegation.js
@@ -0,0 +1,22 @@
+'use strict';
+
+// Babel:
+//import '@babel/polyfill';
+
+function* generator1() {
+	yield 'a';
+	yield 'b';
+	yield 'c';
+}
+
+function* generator2() {
+	yield 1;
+	yield 2
+	yield* generator1();
+	yield 3;
+	yield 4;
+}
+
+for (const item of generator2()) {
+	console.log(item);
+}
