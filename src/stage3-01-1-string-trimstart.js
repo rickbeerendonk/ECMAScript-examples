cd5629d5ee89e8ccd92a31854b75f385ca802881
Rick Beerendonk <rick@beerendonk.com>
2018-07-12 14:42:57 -0500

    Add "Stage 3 - String trimStart() and trimEnd()" examples.

---
+++ src/stage3-01-1-string-trimstart.js
@@ -0,0 +1,6 @@
+const name = '   Hello ECMAScript!   ';
+
+const result = name.trimStart();
+
+console.log('--' + result + '--');
+// --Hello ECMAScript!   --
---
+++ src/stage3-01-2-string-trimend.js
@@ -0,0 +1,6 @@
+const name = '   Hello ECMAScript!   ';
+
+const result = name.trimEnd();
+
+console.log('--' + result + '--');
+// --   Hello ECMAScript!--
