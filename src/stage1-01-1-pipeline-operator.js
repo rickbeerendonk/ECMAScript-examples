37bde22b77a2ff7d3b2512b3b9f81886f97d20ce
Rick Beerendonk <rick@beerendonk.com>
2018-08-21 12:06:27 +0200

    Add Pipeline Operator (stage 1) example.

---
+++ src/stage1-01-1-pipeline-operator.js
@@ -0,0 +1,32 @@
+/*! Mozilla Public License Version 2.0 !*/
+/*! Copyright © 2018 Rick Beerendonk   !*/
+
+// https://github.com/tc39/proposal-pipeline-operator/
+
+'use strict';
+
+function reverse(str) {
+    return str.split('').reverse().join('');
+}
+
+function exclaim(str) {
+    return str + '!';
+}
+
+function capatalize(str) {
+    return str.length === 0 ? str : str[0].toUpperCase() + str.substring(1);
+}
+
+const name = 'ecmascript';
+
+// Old (reverse order of application):
+// const result = exclaim(reverse(capatalize(name)));
+
+// New:
+const result = name
+  |> capatalize
+  |> reverse
+  |> exclaim;
+
+console.log(result);
+// tpircsamcE!
