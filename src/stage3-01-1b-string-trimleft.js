f2be251ca2a4399cb6d684c74d06d4c49be84ce3
Rick Beerendonk <rick@beerendonk.com>
2018-11-25 11:15:11 +0100

    Add trimLeft() and trimRight() alias examples to stage 3.

---
+++ src/stage3-01-1b-string-trimleft.js
@@ -0,0 +1,6 @@
+const name = '   Hello ECMAScript!   ';
+
+const result = name.trimLeft();
+
+console.log('--' + result + '--');
+// --Hello ECMAScript!   --
---
+++ src/stage3-01-2b-string-trimright.js
@@ -0,0 +1,6 @@
+const name = '   Hello ECMAScript!   ';
+
+const result = name.trimRight();
+
+console.log('--' + result + '--');
+// --   Hello ECMAScript!--
