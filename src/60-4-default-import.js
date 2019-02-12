fe8e3390c628e3f26a9df83fa5544aa37b38fafb
Rick Beerendonk <rick@beerendonk.com>
2019-02-11 19:03:44 -0500

    Fix example.

--- src/60-4-default-import.js
+++ src/60-4-default-import.js
@@ -1,7 +1,7 @@
 /*! European Union Public License version 1.2 !*/
 /*! Copyright © 2015 Rick Beerendonk          !*/
 
-import hello, { square, pi } from './60-4-default-export';
+import hello, { square, pi } from './60-4-default-export.js';
 
 console.log(hello()); // 'Hello world!'
 console.log(square(3)); // 9
