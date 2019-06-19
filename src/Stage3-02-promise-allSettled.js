d6ac0d9acb272edfc9738e5f49e749789663d933
Rick Beerendonk <rick@beerendonk.com>
2019-06-19 20:27:15 +0200

    Add "Promise.allSettled()" example.

---
+++ src/Stage3-02-promise-allSettled.js
@@ -0,0 +1,28 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2019 Rick Beerendonk          !*/
+
+/* eslint no-unused-vars:warn */
+
+'use strict';
+
+// Babel:
+//import 'babel-polyfill';
+
+const p1 = () =>
+  new Promise(function(resolve, reject) {
+    setTimeout(() => resolve('Resolved 1'), 2000);
+  });
+const p2 = () =>
+  new Promise(function(resolve, reject) {
+    setTimeout(() => resolve('Resolved 2'), 1000);
+  });
+const p3 = () =>
+  new Promise(function(resolve, reject) {
+    setTimeout(() => reject('Rejected 3'), 1500);
+  });
+
+Promise.allSettled([p1(), p2(), p3()])
+  .then(arr => console.log('Success: ' + JSON.stringify(arr)))
+  .catch(err => console.log('Fail: ' + err));
+
+console.log('End of file...');
