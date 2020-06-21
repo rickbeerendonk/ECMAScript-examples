a6029b0a835532d6692d0c38f5b37e6a181319c0
Rick Beerendonk <rick@beerendonk.com>
2020-06-21 18:42:32 +0200

    Add "ES 2021 - String.replaceAll()" example.

---
+++ src/2021/01-string-replaceall.js
@@ -0,0 +1,7 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2020 Rick Beerendonk          !*/
+
+//github.com/tc39/proposal-string-replaceall
+
+const origin = 'this.is.a.test';
+console.log(origin.replaceAll('.', '__')); // this__is__a__test
