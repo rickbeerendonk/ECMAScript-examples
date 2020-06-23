326c52408165367f77b0f5eb31df0b63d315bc51
Rick Beerendonk <rick@beerendonk.com>
2020-06-23 08:53:46 +0200

    Extend examples.

--- version 5/02 - d. Logical Operators/01-and.js
+++ version 5/02 - d. Logical Operators/01-and.js
@@ -3,4 +3,5 @@
 
 console.log(true && true); // true
 console.log(true && false); // false
+console.log(false && true); // false
 console.log(false && false); // false
--- version 5/02 - d. Logical Operators/02-or.js
+++ version 5/02 - d. Logical Operators/02-or.js
@@ -3,4 +3,5 @@
 
 console.log(true || true); // true
 console.log(true || false); // true
+console.log(false || true); // true
 console.log(false || false); // false
