a51fab48c59701e4c7dad99519e17d2e2c7a29a8
Rick Beerendonk <rick@beerendonk.com>
2021-10-10 10:34:36 +0200

    Add Date example.

---
+++ version 5/08 - Date/01-date.js
@@ -0,0 +1,6 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2021 Rick Beerendonk          !*/
+
+var d = new Date();
+
+console.log(d); // For example: 2021-10-10T08:33:58.560Z
