36a67c5e4d65cffb25473ba7ddfdd4cfb9cca384
Rick Beerendonk <rick@beerendonk.com>
2021-10-10 10:37:39 +0200

    Add "Date - now" example.

---
+++ version 5/08 - Date/02-now.js
@@ -0,0 +1,8 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2021 Rick Beerendonk          !*/
+
+// Milliseconds since 1 January, 1970, 00:00:00 UTC,
+// with leap seconds ignored
+var d = Date.now();
+
+console.log(d); // For example: 1633854993157
