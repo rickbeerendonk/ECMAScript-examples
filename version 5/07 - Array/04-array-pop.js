1d659d429e0031dc42c378533025107854639bad
Rick Beerendonk <rick@beerendonk.com>
2023-01-29 18:25:18 +0100

    Add "Array - Pop" example.

---
+++ version 5/07 - Array/04-array-pop.js
@@ -0,0 +1,9 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2023 Rick Beerendonk          !*/
+
+var a = [true, 2, 'three'];
+
+console.log(a.pop()); // three
+
+console.log(JSON.stringify(a)); // [true,2]
+console.log(a.length); // 2
