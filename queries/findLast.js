9902bb4b02585150299a7a6293a3aa6c28ef7260
Rick Beerendonk <rick@beerendonk.com>
2025-01-23 01:50:49 +0100

    Add Array.findLast() demo.

---
+++ queries/findLast.js
@@ -0,0 +1,8 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2025 Rick Beerendonk          !*/
+
+const arr = [1, 2, 3, 4, 5];
+
+const lastEven = arr.findLast(x => x % 2 === 0);
+
+console.log(lastEven); // 2
