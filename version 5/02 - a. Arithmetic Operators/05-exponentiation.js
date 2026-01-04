0fb89e52e7cb46351afb3a211fdee831e92aee71
Rick Beerendonk <rick@beerendonk.com>
2026-01-04 16:39:10 +0100

    Add "Arithmetic Operators - exponentiation" example.

---
+++ version 5/02 - a. Arithmetic Operators/05-exponentiation.js
@@ -0,0 +1,16 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2026 Rick Beerendonk          !*/
+
+console.log(2 ** 3); // 8
+console.log(5 ** 2); // 25
+console.log(10 ** 0); // 1
+
+var a = 2;
+var b = 10;
+console.log(a ** b); // 1024
+
+console.log(4 ** 0.5); // 2 (square root)
+console.log(8 ** (1/3)); // 2 (cube root, approximately)
+
+console.log((-2) ** 2); // 4
+console.log((-2) ** 3); // -8
