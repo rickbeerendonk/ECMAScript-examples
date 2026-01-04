ae1b4d5453cf2615fccfb02045714ba1f138fc4e
Rick Beerendonk <rick@beerendonk.com>
2026-01-04 16:42:28 +0100

    Add "Bitwise Operators - left shift" example.

---
+++ version 5/02 - e. Bitwise Operators/05-left-shift.js
@@ -0,0 +1,14 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2026 Rick Beerendonk          !*/
+
+// 5 << 1 = 10 (0101 becomes 1010, multiply by 2)
+console.log(5 << 1); // 10
+
+// 5 << 2 = 20 (0101 becomes 10100, multiply by 4)
+console.log(5 << 2); // 20
+
+// 3 << 3 = 24 (0011 becomes 11000, multiply by 8)
+console.log(3 << 3); // 24
+
+// Left shift by 0
+console.log(5 << 0); // 5
