55d572a1a310253b5c78b38ff2bf2f013600dd20
Rick Beerendonk <rick@beerendonk.com>
2026-01-04 16:40:47 +0100

    Add "Assignment Operators - compound" example.

---
+++ version 5/02 - b. Assignment Operators/03-compound-assignment.js
@@ -0,0 +1,26 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2026 Rick Beerendonk          !*/
+
+var x = 10;
+x += 5;
+console.log(x); // 15
+
+var y = 20;
+y -= 3;
+console.log(y); // 17
+
+var z = 4;
+z *= 3;
+console.log(z); // 12
+
+var a = 20;
+a /= 4;
+console.log(a); // 5
+
+var b = 17;
+b %= 5;
+console.log(b); // 2
+
+var s = 'Hello';
+s += ' World';
+console.log(s); // Hello World
