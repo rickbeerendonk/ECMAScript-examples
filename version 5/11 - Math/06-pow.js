c97e4d2f03a9f626031ca54e42ebfc6c17ded42f
Rick Beerendonk <rick@beerendonk.com>
2026-01-17 14:21:08 +0100

    Add "Math - pow" example.

---
+++ version 5/11 - Math/06-pow.js
@@ -0,0 +1,9 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2026 Rick Beerendonk          !*/
+
+console.log(Math.pow(2, 0)); // 1
+console.log(Math.pow(2, 1)); // 2
+console.log(Math.pow(2, 2)); // 4
+console.log(Math.pow(2, 3)); // 8
+console.log(Math.pow(2, -1)); // 0.5
+console.log(Math.pow(2, -2)); // 0.25
