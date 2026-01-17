46cc270cfb6e35a60971c23e4ce60a7a2465db0f
Rick Beerendonk <rick@beerendonk.com>
2026-01-17 14:19:43 +0100

    Add "Math - sqrt" example.

---
+++ version 5/11 - Math/05-sqrt.js
@@ -0,0 +1,8 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2026 Rick Beerendonk          !*/
+
+console.log(Math.sqrt(16)); // 4
+console.log(Math.sqrt(4)); // 2
+console.log(Math.sqrt(2)); // 1.41421...
+console.log(Math.sqrt(0)); // 0
+console.log(Math.sqrt(-1)); // NaN
