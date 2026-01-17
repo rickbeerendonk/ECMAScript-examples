8a1c0c75e84634bb2dcfd98b6d6402e62b83ec3a
Rick Beerendonk <rick@beerendonk.com>
2026-01-17 14:22:43 +0100

    Add "Math - min" example.

---
+++ version 5/11 - Math/08-min.js
@@ -0,0 +1,6 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2026 Rick Beerendonk          !*/
+
+console.log(Math.min(4, 2, 1, 5, 3)); // 1
+console.log(Math.min(1)); // 1
+console.log(Math.min()); // Infinity
