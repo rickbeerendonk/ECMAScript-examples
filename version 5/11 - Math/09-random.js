97d27e1f4945b3c4295940ef2ef6059a65b6e035
Rick Beerendonk <rick@beerendonk.com>
2026-01-17 14:23:15 +0100

    Add "Math - random" example.

---
+++ version 5/11 - Math/09-random.js
@@ -0,0 +1,11 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2026 Rick Beerendonk          !*/
+
+// 1. Random number between 0 (inclusive) and 1 (exclusive)
+console.log(Math.random());
+
+// 2. Random integer between 0 and 9
+console.log(Math.floor(Math.random() * 10));
+
+// 3. Random integer between 1 and 10
+console.log(Math.floor(Math.random() * 10) + 1);
