80f9f446a4c7bc10e416e837595833c3a1032a5f
Rick Beerendonk <rick@beerendonk.com>
2026-01-17 14:14:25 +0100

    Add "Math - abs" example.

---
+++ version 5/11 - Math/01-abs.js
@@ -0,0 +1,7 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2026 Rick Beerendonk          !*/
+
+console.log(Math.abs(-5)); // 5
+console.log(Math.abs(5)); // 5
+console.log(Math.abs(-3.7)); // 3.7
+console.log(Math.abs(-0)); // 0
