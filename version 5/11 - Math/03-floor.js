d60699e0b60bac0d5ee5e235f51cc3a31b7b2c7a
Rick Beerendonk <rick@beerendonk.com>
2026-01-17 14:17:53 +0100

    Add "Math - floor" example.

---
+++ version 5/11 - Math/03-floor.js
@@ -0,0 +1,9 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2026 Rick Beerendonk          !*/
+
+console.log(Math.floor(4.9)); // 4
+console.log(Math.floor(4.1)); // 4
+console.log(Math.floor(4.0)); // 4
+console.log(Math.floor(-4.0)); // -4
+console.log(Math.floor(-4.1)); // -5
+console.log(Math.floor(-4.9)); // -5
