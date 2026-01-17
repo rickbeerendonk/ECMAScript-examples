a4da4cdb22fb5f3b019e473cb9088e1f98725f3c
Rick Beerendonk <rick@beerendonk.com>
2026-01-17 14:16:51 +0100

    Add "Math - round" example.

---
+++ version 5/11 - Math/02-round.js
@@ -0,0 +1,9 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2026 Rick Beerendonk          !*/
+
+console.log(Math.round(4.6)); // 5
+console.log(Math.round(4.5)); // 5
+console.log(Math.round(4.4)); // 4
+console.log(Math.round(-4.4)); // -4
+console.log(Math.round(-4.5)); // -4
+console.log(Math.round(-4.6)); // -5
