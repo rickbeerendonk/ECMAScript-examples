43ed301fd9aca91c55f4021771241f28442ed051
Rick Beerendonk <rick@beerendonk.com>
2026-01-17 14:22:25 +0100

    Add "Math - max" example.

---
+++ version 5/11 - Math/07-max.js
@@ -0,0 +1,6 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2026 Rick Beerendonk          !*/
+
+console.log(Math.max(4, 2, 1, 5, 3)); // 5
+console.log(Math.max(1)); // 1
+console.log(Math.max()); // -Infinity
