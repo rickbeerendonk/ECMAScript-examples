928c6d2ccb4616f529c864933d1c0aee38d92b56
Rick Beerendonk <rick@beerendonk.com>
2026-01-18 10:33:17 -0500

    Add "String - trim" example.

---
+++ version 5/10 - String/09-trim.js
@@ -0,0 +1,7 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2026 Rick Beerendonk          !*/
+
+var str = '  Hello JavaScript  ';
+
+console.log('|' + str + '|'); // |  Hello JavaScript  |
+console.log('|' + str.trim() + '|'); // |Hello JavaScript|
