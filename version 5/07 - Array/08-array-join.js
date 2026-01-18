5906cc482853cf23468a310e1045cd5d9161e9cb
Rick Beerendonk <rick@beerendonk.com>
2026-01-18 11:04:49 -0500

    Add "Array - join" example.

---
+++ version 5/07 - Array/08-array-join.js
@@ -0,0 +1,9 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2026 Rick Beerendonk          !*/
+
+var arr = ['apple', 'banana', 'cherry'];
+
+console.log( arr.join()    ); // apple,banana,cherry
+console.log( arr.join('-') ); // apple-banana-cherry
+console.log( arr.join(' ') ); // apple banana cherry
+console.log( arr.join('')  ); // applebananacherry
