1d6e19cbe6b7c989f29ef86846c57748eae31e2d
Rick Beerendonk <rick@beerendonk.com>
2026-01-18 11:09:24 -0500

    Add "Array - reverse" example.

---
+++ version 5/07 - Array/10-array-reverse.js
@@ -0,0 +1,8 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2026 Rick Beerendonk          !*/
+
+var arr = [1, 2, 3, 4, 5];
+
+arr.reverse(); // No return, original array is changed!
+
+console.log(JSON.stringify(arr)); // [5,4,3,2,1]
