9f5d1bd7f9e468e6f4f52f93be77f9a2e0f052ba
Rick Beerendonk <rick@beerendonk.com>
2026-01-18 10:16:25 -0500

    Improve presentation.

--- version 5/12 - Global Functions/03-isNaN.js
+++ version 5/12 - Global Functions/03-isNaN.js
@@ -1,7 +1,6 @@
 /*! European Union Public License version 1.2 !*/
 /*! Copyright © 2026 Rick Beerendonk          !*/
 
-
 console.log('NaN:',  isNaN( NaN)); // true
 console.log('-NaN:', isNaN(-NaN)); // true
 
@@ -17,6 +16,7 @@ console.log('true:',  isNaN(true));  // false (true coerced to 1)
 console.log('false:', isNaN(false)); // false (false coerced to 0)
 
 // String
+
 console.log('"hello":', isNaN("hello")); // true (string coerced to number becomes NaN)
 console.log('"123":',   isNaN("123"));   // false (string coerced to 123)
 console.log('"":',      isNaN(""));      // false (empty string coerced to 0)
