b28811699a5d83e2c9dbe191b89eb87a634c7053
Rick Beerendonk <rick@beerendonk.com>
2026-01-18 10:31:22 -0500

    Add "String - replace" example.

---
+++ version 5/10 - String/08-replace.js
@@ -0,0 +1,10 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2026 Rick Beerendonk          !*/
+
+var str1 = 'Hello World';
+
+console.log(str1.replace('World', 'JavaScript')); // Hello JavaScript
+console.log(str1.replace('Hello', 'Hi')); // Hi World
+
+var str2 = 'AAA';
+console.log(str2.replace('A', 'B')); // BAA (replaces first only)
