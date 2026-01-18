9efb0719e260ab89b185ebdb90c84df463516544
Rick Beerendonk <rick@beerendonk.com>
2026-01-18 10:45:32 -0500

    Add "String - concat" example.

---
+++ version 5/10 - String/10-concat.js
@@ -0,0 +1,10 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2026 Rick Beerendonk          !*/
+
+var str = "abc";
+console.log(str.concat("def")); // "abcdef"
+console.log(str + "def");       // "abcdef"
+
+// Concat multiple strings
+console.log(str.concat("def", "ghi")); // "abcdefghi"
+console.log(str + "def" + "ghi");      // "abcdefghi"
