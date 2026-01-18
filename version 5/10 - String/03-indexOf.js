3e832c1504cab51f923d84e163a8ab8087fe93c6
Rick Beerendonk <rick@beerendonk.com>
2026-01-18 10:19:00 -0500

    Add "String - indexOf" example.

---
+++ version 5/10 - String/03-indexOf.js
@@ -0,0 +1,10 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2026 Rick Beerendonk          !*/
+
+var str = 'Hello World';
+
+console.log(str.indexOf('o'));     // 4
+console.log(str.indexOf('World')); // 6
+console.log(str.indexOf('xyz'));   // -1
+
+console.log(str.indexOf('o', 5));  // 7 (starts searching from index 5)
