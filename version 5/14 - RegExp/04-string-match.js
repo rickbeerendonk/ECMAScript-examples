68b8513985f37db376a706d4ed4ef1e1a97b2880
Rick Beerendonk <rick@beerendonk.com>
2026-02-15 11:58:12 +0100

    Add "RegExp - match" example.

---
+++ version 5/14 - RegExp/04-string-match.js
@@ -0,0 +1,19 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2026 Rick Beerendonk          !*/
+
+// Without global flag - returns first match
+var str = 'hello123world456';
+console.log(str.match(/\d+/)); // ["123", ...]
+
+// With global flag - returns all matches
+console.log(str.match(/\d+/g)); // ["123", "456"]
+
+// No match returns null
+console.log('hello'.match(/\d+/)); // null
+
+// With capturing groups
+var text = '2015-01-15 2016-02-20';
+console.log(text.match(/(\d{4})-(\d{2})-(\d{2})/)); // ["2015-01-15", "2015", "01", "15"]
+
+// Global with capturing groups
+console.log(text.match(/(\d{4})-(\d{2})-(\d{2})/g)); // ["2015-01-15", "2016-02-20"]
