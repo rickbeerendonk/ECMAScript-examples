4c12e669e7b264205e8a562128791af519bcaccf
Rick Beerendonk <rick@beerendonk.com>
2026-02-18 21:44:56 +0100

    Add "Array - isArray" example.

--- version 5/07 - Array/22-array-isArray.js
+++ version 5/07 - Array/22-array-isArray.js
@@ -3,17 +3,15 @@
 
 // Arrays
 console.log(Array.isArray([1, 2, 3])); // true
-console.log(Array.isArray([])); // true
 
 // Non-arrays
-console.log(Array.isArray("string")); // false
-console.log(Array.isArray(123)); // false
+console.log(Array.isArray('string')); // false
 console.log(Array.isArray({})); // false (object literal)
-console.log(Array.isArray({length: 3})); // false (array-like but not array)
+console.log(Array.isArray({ length: 3 })); // false (array-like but not array)
 
 // Null and undefined
 console.log(Array.isArray(null)); // false
 console.log(Array.isArray(undefined)); // false
 
 // Functions are not arrays
-console.log(Array.isArray(function() {})); // false
+console.log(Array.isArray(function () {})); // false
