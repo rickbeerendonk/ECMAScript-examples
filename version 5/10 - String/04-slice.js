da5c2766aaf85fcb4ffce8523d9d5d919df2c27b
Rick Beerendonk <rick@beerendonk.com>
2026-02-18 15:23:47 +0100

    Add "String - slice" example.

---
+++ version 5/10 - String/04-slice.js
@@ -0,0 +1,12 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2026 Rick Beerendonk          !*/
+
+var str = 'Hello World';
+
+console.log(str.slice(0, 5)); // Hello
+console.log(str.slice(6)); // World
+console.log(str.slice(0, -1)); // Hello Worl
+
+var text = 'JavaScript';
+console.log(text.slice(0, 4)); // Java
+console.log(text.slice(-6)); // Script
