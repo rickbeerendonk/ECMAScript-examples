e8c263915ff34b8c1780c81a69e67941ec772c14
Rick Beerendonk <rick@beerendonk.com>
2026-02-18 21:45:31 +0100

    Add "String - charAt" example.

---
+++ version 5/10 - String/01-charAt.js
@@ -0,0 +1,9 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2026 Rick Beerendonk          !*/
+
+var str = 'Hello';
+
+console.log(str.charAt(0)); // H
+console.log(str.charAt(1)); // e
+console.log(str.charAt(4)); // o
+console.log(str.charAt(10)); // '' (empty string, out of bounds)
